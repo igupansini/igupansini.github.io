894aa8390ff04522e74e6b03858a09e9e752821c
IguPansini <pansini@outlook.com.br>
2020-04-16 00:22:16 -0300

    Responsividade

---
+++ index.js
@@ -0,0 +1,23 @@
+$(document).ready(function (e) {
+    $win = $(window);
+    $navbar = $('#header');
+    $toggle = $('.toggle-button');
+    var width = $navbar.width();
+    toggle_onclick($win, $navbar, width);
+
+    $win.resize(function () {
+        toggle_onclick($win, $navbar, width);
+    });
+
+    $toggle_click(function (e) {
+        $navbar.toggleClass("toggle-left");
+    });
+});
+
+function toggle_onclick($win, $navbar, width) {
+    if ($win.width() <= 768) {
+        $navbar.css({ left: `-${width}px` });
+    } else {
+        $navbar.css({ left: '0px' });
+    }
+}
